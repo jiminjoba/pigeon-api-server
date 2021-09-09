6b92c593276dc2adeed687a1eba6adbc0e72eecc
Daniil <73400755+DannilBurmatov@users.noreply.github.com>
2021-09-10 00:32:35 +0300

    refactoring

--- hashed.js
+++ hashed.js
@@ -4,24 +4,30 @@ const require = createRequire(import.meta.url);
 const bcrypt = require('bcrypt');
 
 
-const genSalt = async () => {
+const genSalt = async () => 
+{
     let salt = await bcrypt.genSalt(10);
     return salt;
 }
 
-const bcryptPassword = async (pass) => {
+const bcryptPassword = async (pass) => 
+{
     let salt = await genSalt();
     const hasedPassword = await bcrypt.hash(pass, salt);
     let hash = hasedPassword;
     return {hash, salt};
 } 
 
-const verifyPassword = async (pass,hash,salt) => {
+const verifyPassword = async (pass,hash,salt) => 
+{
     const hasedPassword = await bcrypt.hash(pass, salt);
-    if (hash === hasedPassword ){
+    if (hash === hasedPassword )
+    {
         console.log("True");
         return true;
-    }else{
+    }
+    else
+    {
         console.log("False");
         return false;
     }
--- main.js
+++ main.js
@@ -10,44 +10,53 @@ const users = [];
 app.use(express.json());
 
 // login 
-app.post('/login', async (req, res)=> {
-    try{
+app.post('/login', async (req, res)=> 
+{
+    try
+    {
         console.log('login');
         const userInDataBase = findUser(req.body.name);
         //!userInData === false тогда сработает if
-        if (!userInDataBase){
+        if (!userInDataBase)
+        {
             res.status(401).send("Login or password invalid ");
             return;
         }
-        if (!await verifyPassword(req.body.password, userInDataBase.hashedPassword, userInDataBase.salt)){
+        if (!await verifyPassword(req.body.password, userInDataBase.hashedPassword, userInDataBase.salt))
+        {
             res.status(401).send("Login or password invalid 1 ");
             return;
         }
         res.json(userInDataBase);
-    }catch(error){
+    }
+    catch(error)
+    {
         console.log(error);
         res.status(500).send();
     }
 })
 
 // find user
-function findUser(userName){
+const findUser = (userName) => {
     const user = users.find(user => user.name === userName);
     return user;
 }
 
 // register new user
 app.post('/users',async (req,res)=> {
-    try{
+    try
+    {
         console.log('register');
         const userInDataBase = findUser(req.body.name);
-        if(userInDataBase != null){
+        if(userInDataBase != null)
+        {
             res.status(400).send("User already exist ");
             return;
         }
         let hash =  await bcryptPassword(req.body.password, req.body.salt);
         console.log(hash);
-        const user = {
+        const user = 
+        {
             name: req.body.name, 
             hashedPassword: hash.hash, 
             password: req.body.password, 
@@ -55,7 +64,9 @@ app.post('/users',async (req,res)=> {
         };
         users.push(user);
         res.status(201).send();
-    }catch(error){
+    }
+    catch(error)
+    {
         console.log(error);
         res.status(500).send();
         
@@ -63,27 +74,34 @@ app.post('/users',async (req,res)=> {
 })
 
 // return all users 
-app.get('/users', (req, res)=> {
+app.get('/users', (req, res) => 
+{
     res.json(users);
 })
 
 // set-password
-app.post('/users/set-password', async (req,res)=>{
-    try{
+app.post('/users/set-password', async (req,res) => 
+{
+    try
+    {
         console.log('set-password');
         const userInDataBase = findUser(req.body.name);
-        if(userInDataBase == null){
+        if(userInDataBase == null)
+        {
             res.status(400).send("Cant find ");
             return;
         }
-        if (req.body.currentPassword !== userInDataBase.password){
+        if (req.body.currentPassword !== userInDataBase.password)
+        {
             res.status(400).send("Invalid password ")
         }
         userInDataBase.password = req.body.password;
         let newHash = await bcryptPassword(userInDataBase.password, userInDataBase.salt);
         userInDataBase.hashedPassword = newHash.hash;
         userInDataBase.salt = newHash.salt;
-    }catch(error){
+    }
+    catch(error)
+    {
         console.log(error);
         res.status(500).send();
     }
